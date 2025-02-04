512d6267d56d04bc0e6c165c7b9d0178babb68f8
vasix <vasixkrin@gmail.com>
2025-02-04 20:57:27 +0200

    Added cart app.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'easy_thumbnails',
     # local
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+CART_SESSION_ID = 'cart'
