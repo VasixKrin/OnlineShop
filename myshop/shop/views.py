a45f731495e09491ee4ac6cc97ed649eca4f8d6a
vasix <vasixkrin@gmail.com>
2025-02-04 20:48:12 +0200

    Added views and html templates for the main page.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, ListView
+from .models import Category, Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/product/list.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = self.kwargs.get('category_slug')
+        category = None
+        categories = Category.objects.all()
+        products = Product.objects.filter(available=True)
+
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+
+        context.update(
+            {
+                'category': category,
+                'categories': categories,
+                'products': products,
+            }
+        )
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/product/detail.html'
+    context_object_name = 'product'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Product, id=self.kwargs['pk'], slug=self.kwargs['slug'], available=True)
